e3723e19bffe8c645036bdfef93bb97fa7530a4e
LindaCr <lindacrook.44@gmail.com>
2021-08-06 23:24:20 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './features/movie/movie-list.component';
+import { ActorListComponent } from './features/actor/actor-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    ActorListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
